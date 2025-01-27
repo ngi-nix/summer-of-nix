34b3b10533dc778ad2fb8a07d2bcc54ce4fb27fb
eljamm <fedi.jamoussi@protonmail.ch>
2025-01-27 11:53:38 +0100

    feat: process fund files directly using python
    Still needs work for using with the proper data-types.

--- admin/automation/process.py
+++ admin/automation/process.py
@@ -1,32 +1,102 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i python3 -p jaq "python3.withPackages(ps: with ps; [ pandas pygithub python-dotenv ])"
+#!nix-shell -i python3 -p jaq "python3.withPackages(ps: with ps; [ pydantic ])"
 
-"""
-Extract useful information from processed grant files
-"""
-
+import argparse
+import json
+import logging
 import os
-import subprocess
+from typing import List, Optional
 
-import pandas as pd
+from pydantic import BaseModel, ValidationError
 
-input = "./funds"
-output = "./info.json"
+
+class Cli:
+    def __init__(self) -> None:
+        self.parser = argparse.ArgumentParser(
+            description="""
+            Extract useful information from processed grant files
+            """
+        )
+        self.parser.add_argument(
+            "-i",
+            "--input",
+            type=dir_path,
+            help="Input directory containing grant files",
+            default="./funds",
+        )
+        self.parser.add_argument(
+            "-o",
+            "--output",
+            # TODO: better message?
+            help="Output file",
+            default="./info.json",
+        )
+        self.parser.add_argument(
+            "-d",
+            "--debug",
+            action="store_true",
+            help="Print debugging information",
+        )
+        self.args = self.parser.parse_args()
+
+
+class Proposal(BaseModel):
+    Name: Optional[str]
+    Websites: List[str]
+    Summary: Optional[str]
+
+
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+
+
+def map_fund_data(input_file: str, all_proposals: List[Proposal]):
+    with open(input_file, "r") as f:
+        data = json.load(f)
+
+    proposals = data.get("fund", {}).get("proposals", [])
+
+    for p in proposals:
+        name = p.get("properties", {}).get("webpage", {}).get("sitename")
+        websites = p.get("proposal", {}).get("websites", {}).get("website", [])
+        summary = p.get("properties", {}).get("webpage", {}).get("summary")
+
+        proposal_data = {"Name": name, "Websites": websites, "Summary": summary}
+
+        try:
+            if name is None:
+                logging.warning(f"Skipping entry due to null name: {proposal_data}")
+                continue
+            all_proposals.append(Proposal(**proposal_data))
+        except ValidationError as e:
+            logger.error(f"Invalid data for proposal: {proposal_data}. Error: {e}")
+
+
+def main(input_dir: str, output_file: str):
+    if not os.path.isdir(input_dir):
+        print(f"Error: '{input_dir}' does not exist or is not a directory.")
+        exit(1)
+
+    all_proposals = []
+
+    for input_file in os.listdir(input_dir):
+        if input_file.endswith(".json"):
+            input_path = os.path.join(input_dir, input_file)
+            map_fund_data(input_path, all_proposals)
+
+    # Write all processed proposals to a single output file
+    with open(output_file, "w") as f:
+        json.dump([proposal.dict() for proposal in all_proposals], f, indent=4)
 
 
 if __name__ == "__main__":
-    subprocess.call(["bash", "./process.sh"])
+    args = Cli().args
 
-    columns = ["Name", "Websites", "Summary"]
-    subgrants = pd.DataFrame(columns=columns)
+    logger = logging.getLogger(__name__)
+    logging_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(level=logging_level)
 
-    for file in os.listdir(input):
-        file_path = os.path.join(input, file)
-
-        if os.path.isfile(file_path) and file.endswith("-processed.json"):
-            df = pd.read_json(file_path)[columns]
-            subgrants = pd.concat([subgrants, df])
-
-    subgrants.drop_duplicates(subset="Name", inplace=True)
-    subgrants.set_index("Name", inplace=True)
-    subgrants.to_json(output, orient="index", indent=2)
+    main(args.input, args.output)
--- admin/automation/shell.nix
+++ admin/automation/shell.nix
@@ -8,6 +8,7 @@ let
       pandas
       pandas-stubs
       pygithub
+      pydantic
     ]
   );
 in
