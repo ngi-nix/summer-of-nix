38ca8238af4f5db97041a96de90ae779f955cb3f
eljamm <fedi.jamoussi@protonmail.ch>
2025-01-28 09:59:59 +0100

    feat: init npins, refactor nix shell

--- admin/automation/shell.nix
+++ admin/automation/shell.nix
@@ -1,21 +1,30 @@
+{
+  sources ? import ./npins,
+  system ? builtins.currentSystem,
+  pkgs ? import sources.nixpkgs { inherit system; },
+}:
 let
-  pkgs = import <nixpkgs> { };
-  export = pkgs.writeShellScriptBin "extract-project-data-from-nlnet" ''
-    ${pkgs.lib.getExe python} ${./process.py}
-  '';
   python = pkgs.python3.withPackages (
     ps: with ps; [
       pandas
       pandas-stubs
-      pygithub
+      pygithub # TODO: remove
       pydantic
+      githubkit
     ]
   );
+  # Commands
+  export = pkgs.writeShellScriptBin "extract-project-data-from-nlnet" ''
+    ${pkgs.lib.getExe python} ${./process.py}
+  '';
+  sync = pkgs.writeShellScriptBin "sync-projects" ''
+    ${pkgs.lib.getExe python} ${./sync.py}
+  '';
 in
 pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    jaq
-    python
+  packages = [
     export
+    python
+    sync
   ];
 }
