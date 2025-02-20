f98d0decb87ef7c2837a56c5fb5e9a9a9559ca24
eljamm <fedi.jamoussi@protonmail.ch>
2025-02-20 09:58:16 +0100

    feat: get new author contacts for messages

--- admin/scripts/extract_project_metadata.py
+++ admin/scripts/extract_project_metadata.py
@@ -4,30 +4,31 @@ import argparse
 import json
 import logging
 import os
+import random
 import sys
+from contextlib import contextmanager
 
 import ijson
+import pandas as pd
+import questionary
 from common.models.dashboard import Fund
-from common.models.notion import Overview, Subgrant
+from common.models.notion import AuthorMessages, Overview, Subgrant
 from common.utils import dir_path, get_notion_projects, remove_urls, zip_file_type
 from pydantic import ValidationError
 from tqdm import tqdm
 
 # fmt: off
 choices = {
-    "default": lambda subgrants: {
+    "default": lambda inputs: {
         s.name: s.model_dump()
-        for s in subgrants
+        for s in inputs["subgrants"]
     },
-    "emails": lambda subgrants: sorted(
-        set(s.contact.email for s in subgrants),
-        key=lambda email: email.split("@")[1]
-    ),
-    "overview": lambda subgrants: {
+    "emails": lambda inputs: get_new_contacts_for_message(**inputs),
+    "overview": lambda inputs: {
         o.name: o.model_dump()
         for o in [
             Overview(name=s.name, websites=s.websites, summary=s.summary)
-            for s in subgrants
+            for s in inputs["subgrants"]
         ]
     },
 }
@@ -82,6 +83,65 @@ class Cli:
         self.args = self.parser.parse_args()
 
 
+@contextmanager
+def redirect_stdout(tmp_stream):
+    """Temporarily redirect sys.stdout to a different output stream"""
+    original_stdout = sys.stdout
+    sys.stdout = tmp_stream
+    try:
+        yield
+    finally:
+        sys.stdout = original_stdout
+
+
+def get_new_contacts_for_message(
+    logger: logging.Logger, args: argparse.Namespace, subgrants: list[Subgrant]
+):
+    """
+    Prompts users to choose a message, then determines which NGI project authors
+    have not been contacted for that message, yet
+    """
+    messages = get_notion_projects(args.author_messages_zip)
+
+    if messages is None:
+        logger.error(f"Failed to extract {args.author_messages_zip}")
+        exit()
+
+    messages = pd.read_csv(messages, usecols=["Name", "Already contacted"])
+    messages["Already contacted"] = messages["Already contacted"].apply(
+        lambda x: remove_urls(x) if pd.notna(x) else ""
+    )
+    messages = {
+        m["Name"]: AuthorMessages(message=m["Name"], contacted=m["Already contacted"])
+        for m in messages.to_dict(orient="records")
+    }
+
+    # Prompt users through stderr since stdout is already used for the result
+    with redirect_stdout(sys.stderr):
+        response: str | None = questionary.select(
+            "What email message do you need?",
+            choices=[n.message for n in messages.values()],
+        ).ask()
+
+    if response is None:
+        exit()
+
+    new_contacts: list[str] = []
+    for c in subgrants:
+        if c.contact.name in messages[response].contacted:
+            continue
+        new_contacts.append(f'"{c.contact.name}","<{c.contact.email}>"')
+    new_contacts = sorted(set(new_contacts))
+
+    if args.samples is not None:
+        new_contacts = random.sample(new_contacts, args.samples)
+
+    print("Name,Email")
+    for c in new_contacts:
+        print(c)
+    exit()
+
+
 def main():
     args = Cli().args
 
@@ -127,7 +187,9 @@ def main():
         except ValidationError as e:
             logger.error(e)
 
-    content = choices[args.preset](subgrants)
+    content = choices[args.preset](
+        {"logger": logger, "args": args, "subgrants": subgrants}
+    )
 
     json.dump(content, sys.stdout, indent=2)
 
