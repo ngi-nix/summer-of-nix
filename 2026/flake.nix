d33053252800cf4333334f7979b0b2317c9ac32d
Ivan Mincik <ivan.mincik@gmail.com>
2026-01-26 10:02:25 +0100

    Add repository report for 2025-01
    Closes: #393

---
+++ 2026/flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      fonts = pkgs.makeFontsConf {
+        fontDirectories = [
+          pkgs.dejavu_fonts
+        ];
+      };
+
+      mkReport =
+        report:
+        pkgs.stdenv.mkDerivation {
+          name = report;
+          src = ./.;
+          buildInputs = with pkgs; [
+            pandoc
+            texlive.combined.scheme-small
+          ];
+          phases = [
+            "unpackPhase"
+            "buildPhase"
+          ];
+          buildPhase = ''
+            export FONTCONFIG_FILE=${fonts}
+            mkdir -p $out
+            pandoc $src/${report}.md \
+                -f gfm \
+                --pdf-engine=xelatex \
+                -M date="`date "+%B %e, %Y"`" \
+                -o $out/${report}.pdf
+          '';
+        };
+    in
+    {
+      packages.${system} = {
+        repository-report-2026 = mkReport "repository-report-2026-01";
+      };
+    };
+}
